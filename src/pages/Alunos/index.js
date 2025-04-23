da6695df67a6dc9e0acd9469b7cfa6d55c77b9f1
Arnaldo-PS <arnaldopereirasilva99@gmail.com>
2025-04-23 10:38:00 -0300

    feat: get alunos

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -2,15 +2,41 @@ import React from "react";
 import "./styles.css";
 import logoCadastro from "../../assets/pngegg (1).png";
 import { Link } from "react-router-dom";
+import api from "../../services/api";
 import { FiEdit, FiUserX, FiXCircle } from "react-icons/fi";
 
 export default function Alunos() {
+  const [nome, setNome] = React.useState("");
+  const [alunos, setAlunos] = React.useState([]);
+
+  const email = localStorage.getItem("email");
+  const token = localStorage.getItem("token");
+
+  const authorization = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  React.useEffect(() => {
+    console.log("Authorization header:", authorization);
+
+    api
+      .get("alunos", authorization)
+      .then((response) => {
+        setAlunos(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar alunos:", error);
+      });
+  }, []);
+
   return (
     <div className="aluno-container">
       <header>
         <img src={logoCadastro} alt="Cadastro" width={50} />
         <span>
-          Bem vindo, <strong>Arnaldo</strong>!
+          Bem vindo, <strong>{email}</strong>!
         </span>
         <Link className="button" to="novo/0">
           Cadastrar aluno
@@ -27,23 +53,25 @@ export default function Alunos() {
       </form>
       <h1>Alunos cadastrados</h1>
       <ul>
-        <li>
-          <b>Nome:</b>Paulo
-          <br />
-          <br />
-          <b>Email:</b> paulo@email.com.br
-          <br />
-          <br />
-          <b>Idade:</b> 22
-          <br />
-          <br />
-          <button type="button">
-            <FiEdit size={25} color="#17202a" />
-          </button>
-          <button type="button">
-            <FiUserX size={25} color="#17202a" />
-          </button>
-        </li>
+        {alunos.map((aluno) => (
+          <li key={aluno.id}>
+            <b>Nome:</b> {aluno.nome}
+            <br />
+            <br />
+            <b>Email:</b> {aluno.email}
+            <br />
+            <br />
+            <b>Idade:</b> {aluno.idade}
+            <br />
+            <br />
+            <button type="button">
+              <FiEdit size={25} color="#17202a" />
+            </button>
+            <button type="button">
+              <FiUserX size={25} color="#17202a" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
